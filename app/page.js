e49cf74cfde4cd359fac5fe8da22ceb26c26edfb
naman-1905 <namanoj19@gmail.com>
2026-02-24 13:38:45 +0530

    feat: Add sections to the landing page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import Hero from '@/components/HeroSection'
 import Navbar from '@/components/Navbar'
-import React from 'react'
+import Projects from '@/components/Projects'
+import WorkExperience from '@/components/WorkExperience'
+import About from '@/components/About'
 
 function LandingPage() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <About/>
+      <WorkExperience/>
+      <Projects/>
     </div>
   )
 }
